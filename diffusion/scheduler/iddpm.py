ab11417691449758d2b9908d34377ef820dbcfcf
lawrence-cj <cjs1020440147@icloud.com>
2025-03-16 23:18:45 +0800

    pre-commit;
    Signed-off-by: lawrence-cj <cjs1020440147@icloud.com>

--- diffusion/scheduler/iddpm.py
+++ diffusion/scheduler/iddpm.py
@@ -14,13 +14,13 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+from diffusion.model import gaussian_diffusion as gd
+
 # Modified from OpenAI's diffusion repos
 #     GLIDE: https://github.com/openai/glide-text2im/blob/main/glide_text2im/gaussian_diffusion.py
 #     ADM:   https://github.com/openai/guided-diffusion/blob/main/guided_diffusion
 #     IDDPM: https://github.com/openai/improved-diffusion/blob/main/improved_diffusion/gaussian_diffusion.py
 from diffusion.model.respace import SpacedDiffusion, space_timesteps
-
-from diffusion.model import gaussian_diffusion as gd
 
 
 def Scheduler(

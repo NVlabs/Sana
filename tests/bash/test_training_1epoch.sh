0d746c87d94caddd9deed33d76da40ccd9c5a89a
Enze Xie <johnny_ez@163.com>
2025-03-16 15:38:42 +0800

    update train.sh
    Signed-off-by: lawrence-cj <cjs1020440147@icloud.com>

--- tests/bash/test_training_1epoch.sh
+++ tests/bash/test_training_1epoch.sh
@@ -9,4 +9,4 @@ bash train_scripts/train.sh configs/sana_config/512ms/ci_Sana_600M_img512.yaml -
 
 bash train_scripts/train.sh configs/sana_config/512ms/ci_Sana_600M_img512.yaml --data.data_dir="[asset/example_data]" --data.type=SanaImgDataset --model.multi_scale=false
 
-bash train_scripts/train.sh configs/sana1-5_config/1024ms/Sana_1600M_1024px_AdamW_fsdp.yaml --data.data_dir="[data/toy_data]" --data.load_vae_feat=true
+bash train_scripts/train.sh configs/sana1-5_config/1024ms/Sana_1600M_1024px_AdamW_fsdp.yaml --data.data_dir="[data/toy_data]" --data.load_vae_feat=true --train.num_epochs=1 --train.log_interval=1

2da18bc86ed0408351e554ecd6ea34e771e6df5c
lawrence-cj <cjs1020440147@icloud.com>
2025-03-28 16:23:51 +0800

    tmp change;
    Signed-off-by: lawrence-cj <cjs1020440147@icloud.com>

--- tests/bash/test_training_1epoch.sh
+++ tests/bash/test_training_1epoch.sh
@@ -6,10 +6,10 @@ huggingface-cli download Efficient-Large-Model/sana_data_public --repo-type data
 huggingface-cli download Efficient-Large-Model/toy_data --repo-type dataset --local-dir ./data/toy_data --local-dir-use-symlinks False
 
 # test offline vae feature
-bash train_scripts/train.sh configs/sana_config/512ms/ci_Sana_600M_img512.yaml --data.load_vae_feat=true
-
-# test online vae feature
-bash train_scripts/train.sh configs/sana_config/512ms/ci_Sana_600M_img512.yaml --data.data_dir="[asset/example_data]" --data.type=SanaImgDataset --model.multi_scale=false
+#bash train_scripts/train.sh configs/sana_config/512ms/ci_Sana_600M_img512.yaml --data.load_vae_feat=true
+#
+## test online vae feature
+#bash train_scripts/train.sh configs/sana_config/512ms/ci_Sana_600M_img512.yaml --data.data_dir="[asset/example_data]" --data.type=SanaImgDataset --model.multi_scale=false
 
 # test FSDP training
 bash train_scripts/train.sh configs/sana1-5_config/1024ms/Sana_1600M_1024px_AdamW_fsdp.yaml --data.data_dir="[data/toy_data]" --data.load_vae_feat=true --train.num_epochs=1 --train.log_interval=1

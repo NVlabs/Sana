f1e4ef41b532503d47598cad71b9c93733439730
junsong <cjs1020440147@icloud.com>
2025-10-28 10:21:20 -0700

    fix bug;

--- tests/bash/training/test_training_all.sh
+++ tests/bash/training/test_training_all.sh
@@ -2,12 +2,12 @@
 set -e
 
 mkdir -p data/data_public
-huggingface-cli download Efficient-Large-Model/sana_data_public --repo-type dataset --local-dir ./data/data_public --local-dir-use-symlinks False
-huggingface-cli download Efficient-Large-Model/toy_data --repo-type dataset --local-dir ./data/toy_data --local-dir-use-symlinks False
-huggingface-cli download Efficient-Large-Model/video_toy_data --repo-type dataset --local-dir ./data/video_toy_data --local-dir-use-symlinks False
+hf download Efficient-Large-Model/sana_data_public --repo-type dataset --local-dir ./data/data_public --local-dir-use-symlinks False
+hf download Efficient-Large-Model/toy_data --repo-type dataset --local-dir ./data/toy_data --local-dir-use-symlinks False
+hf download Efficient-Large-Model/video_toy_data --repo-type dataset --local-dir ./data/video_toy_data --local-dir-use-symlinks False
 
 mkdir -p output/pretrained_models
-huggingface-cli download Efficient-Large-Model/Wan2.1-T2V-1.3B --repo-type model --local-dir ./output/pretrained_models/Wan2.1-T2V-1.3B --local-dir-use-symlinks False
+hf download Efficient-Large-Model/Wan2.1-T2V-1.3B --repo-type model --local-dir ./output/pretrained_models/Wan2.1-T2V-1.3B --local-dir-use-symlinks False
 
 # test offline vae feature
 bash train_scripts/train.sh configs/sana_config/512ms/ci_Sana_600M_img512.yaml --data.load_vae_feat=true
@@ -22,4 +22,4 @@ bash train_scripts/train.sh configs/sana1-5_config/1024ms/Sana_1600M_1024px_Adam
 bash train_scripts/train_scm_ladd.sh configs/sana_sprint_config/1024ms/SanaSprint_1600M_1024px_allqknorm_bf16_scm_ladd.yaml  --data.data_dir="[data/toy_data]" --data.load_vae_feat=true --train.num_epochs=1 --train.log_interval=1
 
 # test FSDP video training
-bash train_video_scripts/train.sh configs/sana_video_config/256ms/Sana_1600M_256px_AdamW_fsdp.yaml --np=2 --train.num_epochs=1 --train.log_interval=1 --train.train_batch_size=1
+bash train_video_scripts/train_video_ivjoint.py configs/sana_video_config/256ms/Sana_1600M_256px_AdamW_fsdp.yaml --np=2 --train.num_epochs=1 --train.log_interval=1 --train.train_batch_size=1
--- tests/bash/training/test_training_video.sh
+++ tests/bash/training/test_training_video.sh
@@ -5,4 +5,4 @@ echo "Setting up test data..."
 bash tests/bash/setup_test_data.sh
 
 echo "Testing FSDP video training"
-bash train_video_scripts/train.sh configs/sana_video_config/Sana_2000M_256px_AdamW_fsdp.yaml --np=2 --train.num_epochs=1 --train.log_interval=1 --train.train_batch_size=1
+bash train_video_scripts/train_video_ivjoint.py configs/sana_video_config/Sana_2000M_256px_AdamW_fsdp.yaml --np=2 --train.num_epochs=1 --train.log_interval=1 --train.train_batch_size=1

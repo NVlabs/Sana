f9e5e42012b6b84308f3bd91f02591f053f86aff
junsong <cjs1020440147@icloud.com>
2025-10-29 02:54:15 -0700

    ivjoint is not supported in ci;

--- tests/bash/training/test_training_video.sh
+++ tests/bash/training/test_training_video.sh
@@ -9,4 +9,4 @@ mkdir -p output/pretrained_models
 hf download Wan-AI/Wan2.1-T2V-1.3B --repo-type model --local-dir ./output/pretrained_models/Wan2.1-T2V-1.3B
 
 echo "Testing FSDP video training"
-bash train_video_scripts/train_video_ivjoint.sh configs/sana_video_config/Sana_2000M_256px_AdamW_fsdp.yaml --np=2 --train.num_epochs=1 --train.log_interval=1 --train.train_batch_size=1
+bash train_video_scripts/train_video_ivjoint.sh configs/sana_video_config/Sana_2000M_256px_AdamW_fsdp.yaml --np=2 --train.num_epochs=1 --train.log_interval=1 --train.train_batch_size=1 --train.joint_training_interval=0

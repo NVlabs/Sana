5b5cc40b92494d75e518c4a316146a28e8686820
junsong <cjs1020440147@icloud.com>
2026-06-15 06:14:06 -0700

    Run SANA-WM stage1 smoke in video CI

--- tests/bash/training/test_training_video.sh
+++ tests/bash/training/test_training_video.sh
@@ -10,3 +10,5 @@ hf download Wan-AI/Wan2.1-T2V-1.3B --repo-type model --local-dir ./output/pretra
 
 echo "Testing FSDP video training"
 bash train_video_scripts/train_video_ivjoint.sh configs/sana_video_config/Sana_2000M_256px_AdamW_fsdp.yaml --np=2 --train.num_epochs=1 --train.log_interval=1 --train.train_batch_size=1 --train.joint_training_interval=0
+
+bash tests/bash/training/test_training_sana_wm_stage1.sh
